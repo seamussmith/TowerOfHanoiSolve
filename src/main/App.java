bc88a136bf0e7b2e25c209ab3a23418790007c35
seamussmith <student.141504@worcesterschools.net>
2021-09-16 08:02:34 -0400

    save

--- src/main/App.java
+++ src/main/App.java
@@ -1,9 +1,32 @@
 package main;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class App
 {
     public static void main(String[] args) 
     {
-        System.out.println("Hello, World!");
+        var tower1 = new Stack<Integer>();
+        tower1.addAll(Arrays.asList(5, 4, 3, 2, 1));
+        var tower2 = new Stack<Integer>();
+        var tower3 = new Stack<Integer>();
+        solve(tower1, tower2, tower3, tower1.size());
+        System.out.println(tower2);
+        System.out.println(tower3);
     }
+
+    static void solve(Stack<Integer> tower1, Stack<Integer> tower2, Stack<Integer> tower3, int initalSize)
+    {
+        if (tower2.size() == initalSize || tower3.size() == initalSize)
+            return;
+        var peek1 = tower1.peek();
+        var peek2 = tower2.peek();
+        var peek3 = tower3.peek();
+        if (peek1 != null)
+        {
+            
+        }
+    }
+
 }
